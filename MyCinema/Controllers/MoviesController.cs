c336bced6dde3bebe43ef5b5596fc3833cf0bf5b
Max <addergtx@gmail.com>
2021-07-05 23:18:18 +0300

    Add project files.

---
+++ MyCinema/Controllers/MoviesController.cs
@@ -0,0 +1,84 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyCinema.Data;
+using MyCinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCinema.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private CinemaDbContext _dbContext;
+
+        public MoviesController(CinemaDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_dbContext.Movies);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            var movie = _dbContext.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return NotFound("No record found  against this Id");
+            }
+
+            return Ok(movie);
+        }
+
+        [HttpPost]
+        public IActionResult Post([FromBody] Movie movie)
+        {
+            _dbContext.Movies.Add(movie);
+            _dbContext.SaveChanges();
+
+            return StatusCode(StatusCodes.Status201Created);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, [FromBody] Movie movieObj)
+        {
+            var movie = _dbContext.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return NotFound("No record found  against this Id");
+            }
+
+            movie.Name = movieObj.Name;
+            movie.Language = movieObj.Language;
+            
+            _dbContext.SaveChanges();
+            return Ok("Record update successfully");
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var movie = _dbContext.Movies.Find(id);
+
+            if (movie == null)
+            {
+                return NotFound("No record found  against this Id");
+            }
+
+            _dbContext.Movies.Remove(movie);
+            _dbContext.SaveChanges();
+
+            return Ok("Record deleted");
+        }
+    }
+}
---
+++ MyCinema/Data/CinemaDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyCinema.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyCinema.Data
+{
+    public class CinemaDbContext : DbContext
+    {
+        public CinemaDbContext(DbContextOptions<CinemaDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
