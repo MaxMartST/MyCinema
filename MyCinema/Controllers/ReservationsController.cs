33c83c924b12ef788f84a19f445a2e983d704032
Max <addergtx@gmail.com>
2021-07-19 22:00:57 +0300

    Реализовал логику получения всех бронь. Получение брони по Id, удаление брони

--- MyCinema/Controllers/ReservationsController.cs
+++ MyCinema/Controllers/ReservationsController.cs
@@ -32,5 +32,67 @@ namespace MyCinema.Controllers
 
             return StatusCode(StatusCodes.Status201Created);
         }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet]
+        public IActionResult GetReservation()
+        {
+            var reservations = from reservation in _dbContext.Reservations
+                               join customer in _dbContext.Users on reservation.UserId equals customer.Id
+                               join movie in _dbContext.Movies on reservation.MovieId equals movie.Id
+                               select new
+                               {
+                                   Id = reservation.Id,
+                                   ReservationTime = reservation.ReservationTime,
+                                   CustomerName = customer.Id,
+                                   MovieName = movie.Name
+                               };
+
+            return Ok(reservations);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpGet("{id}")]
+        public IActionResult GetReservationDetail(int id)
+        {
+            var reservationResult = (from reservation in _dbContext.Reservations
+                               join customer in _dbContext.Users on reservation.UserId equals customer.Id
+                               join movie in _dbContext.Movies on reservation.MovieId equals movie.Id
+                               where reservation.Id == id 
+                               select new
+                               {
+                                   Id = reservation.Id,
+                                   ReservationTime = reservation.ReservationTime,
+                                   CustomerName = customer.Id,
+                                   MovieName = movie.Name,
+                                   Email = customer.Email,
+                                   Qty = reservation.Qty,
+                                   Price = reservation.Price,
+                                   Phone = reservation.Phone,
+                                   PlaingDate = movie.PlayingDate,
+                                   PlaingTime = movie.PlayingTime
+                               }).FirstOrDefault();
+
+            return Ok(reservationResult);
+        }
+
+        [Authorize(Roles = "Admin")]
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            var reservation = _dbContext.Reservations.Find(id);
+
+            if (reservation == null)
+            {
+                return NotFound("No record found against this Id");
+            }
+            else
+            {
+                _dbContext.Reservations.Remove(reservation);
+                _dbContext.SaveChanges();
+
+                return Ok("Record deteled");
+            }
+        }
     }
 }
